798f584fefd44338953ec2ead4d91573bdb02fac
Rafael Galleani <rafegal@gmail.com>
2021-07-07 00:43:11 +0100

    added celery beat and celery work to up schedule and ticket engine

---
+++ app/start_celery_beat.sh
@@ -0,0 +1,1 @@
+celery -A ia_engine beat -s /var/run/celery/celerybeat-schedule -l ERROR --pidfile=/var/run/celery/celerybeat.pid
---
+++ app/start_celery_workers.sh
@@ -0,0 +1,4 @@
+celery multi start search-new-schedule -A ia_engine -l ERROR --concurrency=1 -n %h -Q search-new-schedule --pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
+celery multi start search-pending-tickets -A ia_engine -l ERROR --concurrency=1 -n %h -Q search-pending-tickets --pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
+celery multi start execute-schedule -A ia_engine -l ERROR --concurrency=4 -n %h -Q execute-schedule --pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
+celery multi start process-ticket -A ia_engine -l ERROR --concurrency=4 -n %h -Q process-ticket --pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
