a0844ec18fa738468006460d89a526f919eb7781
Rafael Galleani <rafegal@gmail.com>
2021-07-30 23:15:26 +0100

    commented search_pending_ticket worker because this worker will be descontinued

--- app/task_engine/tasks.py
+++ app/task_engine/tasks.py
@@ -1,6 +1,11 @@
+from django.utils import timezone
+
 from celery.decorators import task
-from datetime import timedelta
 from celery.utils.log import get_task_logger
+
+from cron_validator import CronScheduler
+from datetime import timedelta
+
 from task_engine.models import (
     Schedule,
     ScheduleEnvironmentVariable,
@@ -17,10 +22,6 @@ from task_engine.choices import (
     EXECUTION_STATUS_ERROR,
     EXECUTION_STATUS_QUEUE,
 )
-from datetime import datetime
-from django.utils import timezone
-from cron_validator import CronScheduler
-
 
 logger = get_task_logger(__name__)
 
@@ -28,12 +29,19 @@ logger = get_task_logger(__name__)
 @task
 def search_new_schedule():
     logger.info("starting task sync_new_calls")
-    # validar se faz sentido scria um schedule execution para nao criar execução quando um schedule ainda estiver sendo
-    # executado
+    # validar se faz sentido criae uma validacao se tem um schedule execution a correr
+    #  para nao criar execução quando um schedule ainda estiver sendo executado
     for schedule in Schedule.objects.filter(active=True):
         if schedule.cron:
             scheduler = CronScheduler(schedule.cron)
-            if scheduler.time_for_execution():
+            cron_execution_time = scheduler.next_execution_time.strftime(
+                "%Y-%m-%d %H:%M"
+            )
+            if (
+                scheduler.time_for_execution()
+                and cron_execution_time
+                != schedule.last_execution.strftime("%Y-%m-%d %H:%M")
+            ):
                 execute_schedule.delay(schedule.id)
         elif (
             schedule.last_execution is None
@@ -57,14 +65,12 @@ def execute_schedule(schedule_id):
     parameters = {"ENV": {}}
     for environment_variable in environment_variables:
         parameters["ENV"][environment_variable.name] = environment_variable.load_value
-    print(parameters)
     logger.info(schedule.name)
-    print(schedule.name)
     status, data_list, execution_log = schedule.script.execute(parameters)
     schedule_execution_status = (
         EXECUTION_STATUS_SUCCESS if status else EXECUTION_STATUS_ERROR
     )
-    ScheduleExecution.objects.create(
+    schedule_execution = ScheduleExecution.objects.create(
         schedule=schedule,
         execution_status=schedule_execution_status,
         execution_log=execution_log,
@@ -74,26 +80,28 @@ def execute_schedule(schedule_id):
         and StepSchedule.objects.filter(schedule=schedule).exists()
     ):
         for data in data_list:
-            ticket = Ticket.objects.create(schedule=schedule)
+            ticket = Ticket.objects.create(
+                schedule=schedule, schedule_execution=schedule_execution
+            )
             for key, value in data.items():
                 TicketParameter.objects.create(ticket=ticket, name=key, value=value)
+            ticket.execution_status = EXECUTION_STATUS_QUEUE
+            ticket.save()
+            process_ticket.delay(ticket.id)
 
-    print(data_list)
-    print("####")
-    print(execution_log)
     schedule.last_execution = timezone.now()
     schedule.save()
-    return "aew"
 
 
-@task
-def search_pending_tickets():
-    tickets = Ticket.objects.filter(execution_status=EXECUTION_STATUS_PENDING)
-    for ticket in tickets:
-        ticket.execution_status = EXECUTION_STATUS_QUEUE
-        ticket.save()
-        print(ticket.execution_status)
-        process_ticket.delay(ticket.id)
+# @task
+# def search_pending_tickets():
+#     tickets = Ticket.objects.filter(execution_status=EXECUTION_STATUS_PENDING)
+#     for ticket in tickets:
+#         ticket.execution_status = EXECUTION_STATUS_QUEUE
+#         ticket.save()
+#         print(ticket.execution_status)
+#         process_ticket.delay(ticket.id)
+
 
 @task
 def process_ticket(ticket_id):
