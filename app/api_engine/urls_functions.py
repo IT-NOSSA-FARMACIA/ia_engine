683e0fcc6dc2664a1b8b3422e9f0643380454905
Rafael Galleani <rafegal@gmail.com>
2021-09-26 18:34:40 +0100

    fixed fail to open documentation

--- app/api_engine/urls_functions.py
+++ app/api_engine/urls_functions.py
@@ -5,5 +5,5 @@ app_name = "api_engine_functions"
 
 urlpatterns = [
     path("<str:domain>/<str:function_url>/", execute_function, name="execute-function"),
-    path("<str:domain>/<str:function_url>/doc", get_openapi_function, name="execute-function")
+    path("<str:domain>/<str:function_url>/doc/", get_openapi_function, name="execute-function")
 ]
