82c4848dae974a417b1289c75eb2d83438192e69
Rafael Galleani <rafegal@gmail.com>
2022-01-18 22:59:12 +0000

    changed to improve the performance

--- app/core/templatetags/menu_extras.py
+++ app/core/templatetags/menu_extras.py
@@ -6,15 +6,15 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def active(context, pattern_or_urlname):
+    path = ""
+    app_names = context["request"].resolver_match.app_names
+    if app_names:
+        path = app_names[0] + ":"
+    path += context["request"].resolver_match.url_name
+
     for pattern_url in pattern_or_urlname.split(","):
         pattern_url = pattern_url.rstrip().lstrip()
         pattern = pattern_url
-        path = ""
-        app_names = context["request"].resolver_match.app_names
-        if app_names:
-            path = app_names[0] + ":"
-        path += context["request"].resolver_match.url_name
-
         if path == pattern:
             return "active"
     return ""
