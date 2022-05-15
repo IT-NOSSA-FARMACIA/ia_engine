9a81b30c06ea5ae50ccfd739cc77da960ec6e5e4
Rafael Galleani <galleani@MacBook-Pro-de-Rafael.local>
2022-05-15 01:11:55 +0100

    added new commands to create workers by team and changed the fixed concurrency per autoscale

--- app/start_celery_workers.sh
+++ app/start_celery_workers.sh
@@ -1,4 +1,5 @@
 celery multi start search-new-schedule -A ia_engine -l ERROR --concurrency=1 -n %h -Q search-new-schedule --pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
-celery multi start execute-schedule -A ia_engine -l ERROR --concurrency=4 -n %h -Q execute-schedule --pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
-celery multi start process-ticket -A ia_engine -l ERROR --concurrency=4 -n %h -Q process-ticket --pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
-celery multi start notify-execution -A ia_engine -l ERROR --concurrency=4 -n %h -Q notify-execution --pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
+celery multi start execute-schedule -A ia_engine -l ERROR --autoscale=4,1 -n %h -Q execute-schedule --pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
+celery multi start process-ticket -A ia_engine -l ERROR --autoscale=4,1 -n %h -Q process-ticket --pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
+celery multi start notify-execution -A ia_engine -l ERROR --autoscale=4,1 -n %h -Q notify-execution --pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
+python manage.py start_celery_workers_by_team
